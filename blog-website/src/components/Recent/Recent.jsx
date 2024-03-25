292e82fe9b8da89ef86f20bed1fd01cbed66f4d5
buddhadeb <jeetkoner36@gmail.com>
2024-03-25 12:30:24 +0530

    recent section

---
+++ blog-website/src/components/Recent/Recent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Recent.css'
+
+export default function Recent() {
+    return (
+        <>
+            <div className='Recent-story-feed'>
+                <div className="trending-text">
+                    <img src="/trending-icon.png" className='trending-ison-img' alt="." />
+                    <h2>Trending on Medium</h2>
+                </div>
+            </div>
+        </>
+    )
+}
--- blog-website/src/pages/Home/Home.jsx
+++ blog-website/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Home.css'
 import Hero from '../../components/Hero/Hero'
+import Recent from '../../components/Recent/Recent'
 
 export default function Home() {
   return (
     <>
     <Hero />
+    <Recent />
     </>
   )
 }
