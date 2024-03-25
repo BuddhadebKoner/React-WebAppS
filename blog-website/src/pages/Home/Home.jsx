58044a153211af7a24e6dd9b8548c226dba2a1d3
buddhadeb <jeetkoner36@gmail.com>
2024-03-25 21:26:27 +0530

    nav

--- blog-website/src/pages/Home/Home.jsx
+++ blog-website/src/pages/Home/Home.jsx
@@ -1,36 +1,13 @@
-import React, { useState } from 'react';
-import './Home.css';
-import Hero from '../../components/Hero/Hero';
-import Recent from '../../components/Recent/Recent';
+import React from 'react'
+import './Home.css'
+import Hero from '../../components/Hero/Hero'
+import Recent from '../../components/Recent/Recent'
 
 export default function Home() {
-    const [backgroundImage, setBackgroundImage] = useState('./heroimage.jpg');
-
-    const handleMenuChange = (menu) => {
-      console.log("Menu clicked:", menu);
-      switch (menu) {
-          case "OurStory":
-              setBackgroundImage('./heroimage.jpg');
-              break;
-          case "Membership":
-              setBackgroundImage('./membership.jpg');
-              break;
-          case "WriteBlog":
-              setBackgroundImage('./ourstory.jpg');
-              break;
-          case "Contactus":
-              setBackgroundImage('./write.jpg');
-              break;
-          default:
-              setBackgroundImage('./heroimage.jpg');
-      }
-  };
-  
-
-    return (
-        <>
-            <Hero backgroundImage={backgroundImage} />
-            <Recent />
-        </>
-    );
+  return (
+    <>
+    <Hero />
+    <Recent />
+    </>
+  )
 }
